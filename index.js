36121f9aea163efb7f0c2750b1fcb0e3a992497c
Rafaelmkp <rafaelmobarack@gmail.com>
2021-01-30 19:38:37 -0300

    app config

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 import winston from 'winston';
+import productsRouter from './routes/products.js';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
 global.logger = winston.createLogger({
   level: 'silly',
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'my-grades-api.log' }),
+    new winston.transports.File({ filename: 'products-api.log' }),
   ],
-  format: combine(label({ label: 'my-grades-api' }), timestamp(), myFormat),
+  format: combine(label({ label: 'products-api' }), timestamp(), myFormat),
 });
+
+const { readFile, writeFile } = fs;
 
 const app = express();
 app.use(express.json());
+app.use('/products', productsRouter);
 
-app.listen(3000, () => {
-  console.log('API Started.');
+app.listen(3000, async () => {
+  try {
+    await readFile('products.json');
+    logger.info('API Started!');
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      products: [],
+    };
+    try {
+      writeFile('products.json', JSON.stringify(initialJson));
+      logger.info('API Started and File Created!');
+    } catch (err) {
+      logger.error(err);
+    }
+  }
 });
---
+++ routes/products.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+const router = express.Router();
+
+export default router;
