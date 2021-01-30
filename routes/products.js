d67971d49d084318e89936686cc4c6cc1477de0b
Rafaelmkp <rafaelmobarack@gmail.com>
2021-01-30 19:46:07 -0300

    products router init

--- routes/products.js
+++ routes/products.js
@@ -4,4 +4,9 @@ import { promises as fs } from 'fs';
 const { readFile, writeFile } = fs;
 const router = express.Router();
 
+router.use((err, req, res, next) => {
+  global.logger.error(`${req.method} ${req.baseUrl} ${err.message}`);
+  res.status(400).send({ error: err.message });
+});
+
 export default router;
